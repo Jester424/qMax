5e0bfe2be94557a6466b48d2178a73db7b4826d3
Nicholas Nicosia <nnicosi@wgu.edu>
2025-12-10 20:38:20 -0800

    Deserialize JSON response

--- qMax/Program.cs
+++ qMax/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using static System.Net.WebRequestMethods;
 
 namespace qMax
 {
+    public class TorrentInfo
+    {
+        [JsonPropertyName("hash")]
+        public string Hash { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("num_complete")]
+        public int NumSeeds { get; set; }
+    }
+
     class Program
     {
         static async Task Main(string[] args)
         {
-            string url = "http://localhost:8080/api/v2/app/version";
+            string apiResponse = null;            
+
+            //Example API
+            // /api/v2/torrents/info?filter=downloading&category=sample%20category&sort=ratio
+
+            string url = "http://localhost:8080/api/v2/torrents/info";
 
             using HttpClient client = new HttpClient();
 
@@ -21,15 +43,28 @@ namespace qMax
 
                 response.EnsureSuccessStatusCode();
 
-                string responseBody = await response.Content.ReadAsStringAsync();
-
-                Console.WriteLine("Response:");
-                Console.WriteLine(responseBody);
-                Console.Read();
+                apiResponse = await response.Content.ReadAsStringAsync();
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Request error: {ex.Message}");
+            }
+
+            if (apiResponse == null)
+            {
+                Console.WriteLine("No response received.");
+                return;
+            }
+
+            var torrents = JsonSerializer.Deserialize<List<TorrentInfo>>(apiResponse);
+
+            foreach (var t in torrents)
+            {
+                Console.WriteLine($"ID: {t.Hash}");
+                Console.WriteLine($"Name: {t.Name}");
+                Console.WriteLine($"State: {t.State}");
+                Console.WriteLine($"Seeders: {t.NumSeeds}");
+                Console.WriteLine();
             }
         }
     }
